d23965fd65fa6e83d12e2d5655aa48841c80c523
Youritch <86657843+Youritch@users.noreply.github.com>
2023-03-03 11:40:31 +0100

    gros commit sa mere

--- html/index.php
+++ html/index.php
@@ -3,13 +3,15 @@
 	<head>
 		<title>Onearth</title>
 		<link rel="stylesheet" href="styles/style.css">
+        <link href="/dist/output.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
 	</head>
 	<body>
-        <h1>En cours</h1>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
 		<div class="flex-container">
             <?php
                 $i = 1;
-                while ($i <= 3) {
+                while ($i <= 16) {
                     if ($i<10) {
                         echo '<a src=""><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict" /></a>';
                     } else {
@@ -23,7 +25,7 @@
         <div class="flex-container">
             <?php
                 $i = 4;
-                while ($i <= 6) {
+                while ($i <= 8) {
                     if ($i<10) {
                         echo '<a src=""><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict" /></a>';
                     } else {
@@ -47,12 +49,12 @@
                 }
             ?>
 		</div>
-        <div class="end-page">
-            <nav>
-                    <a src=""><img src="images/house.jpg" class="icons"></a>
-                    <a src=""><img src="images/loupe.png" class="icons"></a>
-                    <a src=""><img src="images/profil.png" class="icons"></a>
+        <footer>
+            <nav class="">
+                    <a href="./images/loupe.png"><span class="material-icons">home</span></a>
+                    <a href="https://www.w3schools.com/csS/css_navbar.asp"><span class="material-icons">search</span> </a>
+                    <a href=""><span class="material-icons">perm_identity</span> </a>
             </nav>
-        </div>
+        </footer>
     </body>
 </html>
