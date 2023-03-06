0bf7d932292f22d3715bd89f17e8fd31c6784393
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 16:13:24 +0100

    commit

--- html/index.php
+++ html/index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 	<head>
 		<title>Onearth</title>
+        <link rel="shortcut icon" type="image/png" href="ico.png" >
 		<link rel="stylesheet" href="styles/style.css">
-        <link rel="stylesheet" href="/dist/output.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <script type="text/javascript" src="javascript.js"></script>
 	</head>
@@ -12,56 +12,12 @@
             <h1>Onearth</h1>
             <button onclick="init_progress()"><span class="material-icons">settings</span></button>
         </header>
-        <h2 style="margin-top: 10px">In progress</h2>
-		<div class="flex-container">
-            <?php
-                $i = 1;
-                while ($i <= 17) {
-                    if ($i<10) {
-                        echo '<button onclick="select_goal(0'.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict" /></button>';
-                    } else {
-                        echo '<button onclick="select_goal('.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict" /></button>';
-                    }
-                    $i ++ ;
-                }
-            ?>
-		</div>
-        <h2>For you</h2>
-        <div class="flex-container">
-            <?php
-                $i = 4;
-                while ($i <= 8) {
-                    if ($i<10) {
-                        echo '<button onclick="select_goal(0'.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict" /></button>';
-                    } else {
-                        echo '<button onclick="select_goal('.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict" /></button>';
-                    }
-                    $i ++ ;
-                }
-            ?>
-		</div>
-        <h2>Done</h2>
-        <div class="flex-container">
-            <?php
-                $i = 7;
-                while ($i <= 9) {
-                    if ($i<10) {
-                        echo '<button onclick="select_goal(0'.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict" /></button>';
-                    } else {
-                        echo '<button onclick="select_goal('.$i.')"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict" /></button>';
-                    }
-                    $i ++ ;
-                }
-            ?>
-		</div>
-        <div style="height: 23%">
-            <p>cc</p>
-        </div>
+        <?php include("./pages/home.php"); ?>
     </body>
     <footer>
         <div id="progress-status">
             <p id="progress-bar-text">0%</p>
-            <p id="progress-bar" style="width: 0%"></p>
+            <div id="progress-bar" style="width: 0%"></div>
         </div>
         <nav class="">
                 <button onclick="init_progress(10)"><span class="material-icons">home</span></button>
--- html/javascript.js
+++ html/javascript.js
@@ -8,7 +8,7 @@ function init_progress(pourcent) {
 			let pBar = document.getElementById("progress-bar");
 			if (parseInt(pBar.style.width.slice(0, pBar.style.width.length - 1)) >= pourcent) {
 				clearInterval(identity);
-			} else if (parseInt(document.getElementById("progress-bar").style.width.slice(0, document.getElementById("progress-bar").style.width.length - 1)) >= 100) {
+			} else if (parseInt(pBar.style.width.slice(0, pBar.style.width.length - 1)) >= 100) {
 				clearInterval(identity);
 			} else {
 				pBar.style.width = parseInt(pBar.style.width.slice(0, pBar.style.width.length - 1)) + 1 + "%";
@@ -24,5 +24,5 @@ function init_progress(pourcent) {
  * @param {number} goal - le goal selectioner (de 0 à 16)
  */
 function select_goal(goal) {
-	return alert("Vous avez cliquer sur le goal n°" + goal);
+	return alert("You selected the " + goal+"th goal.");
 }
