8b0e23fc3b6115d276c3a0b2e181604b7d278ca3
Youritch <86657843+Youritch@users.noreply.github.com>
2023-03-06 17:25:56 +0100

    correction

--- html/pages/home.php
+++ html/pages/home.php
@@ -1,45 +1,44 @@
-<section class="home-page">
-<h2 style="margin-top: 10px">In progress</h2>
-<div class="flex-container">
-                <?php
-                    $i = 1;
-                    while ($i <= 17) {
-                        if ($i<10) {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        } else {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        }
-                        $i ++ ;
-                    }
-                ?>
-            </div>
-            <h2>For you</h2>
-            <div class="flex-container">
-                <?php
-                    $i = 4;
-                    while ($i <= 8) {
-                        if ($i<10) {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        } else {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        }
-                        $i ++ ;
-                    }
-                ?>
-            </div>
-            <h2>Done</h2>
-            <div class="flex-container">
-                <?php
-                    $i = 7;
-                    while ($i <= 9) {
-                        if ($i<10) {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        } else {
-                            echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p><div></button>';
-                        }
-                        $i ++ ;
-                    }
-                ?>
-            </div>
-    <div style="height: 200px;"></div>
+<section>
+    <h2 style="margin-top: 10px">In progress</h2>
+    <div class="flex-container">
+        <?php
+            $i = 1;
+            while ($i <= 17) {
+                if ($i<10) {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                } else {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                }
+                $i ++ ;
+            }
+        ?>
+    </div>
+    <h2>For you</h2>
+    <div class="flex-container">
+        <?php
+            $i = 4;
+            while ($i <= 8) {
+                if ($i<10) {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                } else {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                }
+                $i ++ ;
+            }
+        ?>
+    </div>
+    <h2>Done</h2>
+    <div class="flex-container">
+        <?php
+            $i = 7;
+            while ($i <= 12) {
+                if ($i<10) {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><span class="goal'.$i.' material-icons-round" style="display: flex">task_alt</span><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-0'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                } else {
+                    echo '<button onclick="goal_progress('.$i.')" class="stg-button goal'.$i.'"><span class="goal'.$i.' material-icons-round" style="display: flex">task_alt</span><img src="https://sdgs.un.org/sites/default/files/goals/E_SDG_Icons-'.$i.'.jpg" class="sdg-pict goal'.$i.'" /><div class="stg-progress-container goal'.$i.'"><p class="stg-progress-text goal'.$i.'">0/5</p><p class="stg-progress goal'.$i.'" style="width: 0%"></p></div></button>';
+                }
+                $i ++ ;
+            }
+        ?>
+    </div>
 </section>
