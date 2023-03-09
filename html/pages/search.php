196f091ed78c7d06883c7f0cedf8532fd3b21588
Youritch <86657843+Youritch@users.noreply.github.com>
2023-03-09 17:12:23 +0100

    debut btn search

--- html/pages/search.php
+++ html/pages/search.php
@@ -1,5 +1,6 @@
 <section id="search" class="pages" style="display: none">
     <div class="searchbar">
         <input type="text" placeholder="Search..">
+        <button onclick="search()" id="srh-button"><span class="material-icons" style="color: #252525">search</span>Search</button>
     </div>
 </section>
