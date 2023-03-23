af84df036b87f4ba703832d72f2eb409dbc61a8f
Cleboost <clement.balarot@gmail.com>
2023-03-23 13:59:33 +0100

    add active route

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,14 +1,15 @@
 <template>
    <nav class=" p-6 flex flex-raw bg-[#252525] text-white h-[15%] w-full fixed bottom-0 justify-center items-center">
-      <RouterLink to="/" class="flex w-1/3 justify-center"><span class="material-icons-round text-[100px]">home</span></RouterLink>
-      <RouterLink to="/search" class="flex w-1/3 justify-center"><span class="material-icons-round text-[100px]">search</span></RouterLink>
-      <RouterLink to="/friends" class="flex w-1/3 justify-center"><span class="material-icons-round text-[100px]">perm_identity</span></RouterLink>
+      <RouterLink to="/" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[100px]">home</span></RouterLink>
+      <RouterLink to="/search" class="flex w-1/3 justify-center" exact-active-class="activeRoute" active-class="activeRoute"><span class="material-icons-round text-[100px]">search</span></RouterLink>
+      <RouterLink to="/friends" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[100px]">perm_identity</span></RouterLink>
    </nav>
 </template>
 
 <style scoped>
-   nav a:active {
-      transform: scale(0.90);
-      transition: 0.2s;
-   }
+  .activeRoute {
+    color: #42b983;
+    transform: scale(1.2);
+    transition: 0.2s
+  }
 </style>
