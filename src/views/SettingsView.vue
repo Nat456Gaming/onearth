5552777772bf825fa906c7b58302300c96e02262
Cleboost <clement.balarot@gmail.com>
2023-03-23 14:00:24 +0100

    init settings vue

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This page is in developpement !</h1>
+</template>
+
+<style scoped>
+  h1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
