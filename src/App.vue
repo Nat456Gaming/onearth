7291b2faa050e8393aff2794cb80ad3999d94e8b
Cleboost <clement.balarot@gmail.com>
2023-03-23 13:59:21 +0100

    clean import

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import MenuHeader from './components/HeaderMenu.vue'
 import MenuFooter from './components/FooterMenu.vue'
 </script>
