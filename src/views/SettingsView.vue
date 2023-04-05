10673be6d6bb6703f485cba687c5b590e5b52ede
Cleboost <clement.balarot@gmail.com>
2023-04-05 18:39:30 +0200

    add param

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -5,20 +5,22 @@
 <template>
 	<!-- <img src="https://www.gif-maniac.com/gifs/51/50643.gif" class=" mt-44 h-[55%] w-full" v-bind:class="{ hidden: !loading }"> -->
 
-	<div class="bb z-50" v-bind:class="{ hidden: !loading }"></div>
+	<div class="loading z-50" v-bind:class="{ hidden: !loading }"></div>
 	<!-- Toutes les bannière  -->
-	<!-- connection reussie -->
-	<div class="alert alert-success shadow-lg mt-[20%]" v-bind:class="{ hidden: !succesMessage }">
-		<div>
-			<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-			<span>You have been successfully connected !</span>
+	<div class="top-[12%] fixed w-full flex flex- justify-center">
+		<!-- connection reussie -->
+		<div class="alert alert-success shadow-lg w-3/4" v-bind:class="{ hidden: !succesMessageState }" @click="this.succesMessageState = false">
+			<div>
+				<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+				<span>{{ succesMessage }}</span>
+			</div>
 		</div>
-	</div>
-  <!-- connection echoue -->
-	<div class="alert alert-error shadow-lg" v-bind:class="{ hidden: !errorMessage }">
-		<div>
-			<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-			<span>Connection failed successfully.</span>
+		<!-- connection echoue -->
+		<div class="alert alert-error shadow-lg w-3/4" v-bind:class="{ hidden: !errorMessageState }" @click="this.errorMessageState = false">
+			<div>
+				<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+				<span>{{ errorMessage }}</span>
+			</div>
 		</div>
 	</div>
 
@@ -28,21 +30,23 @@
 		<div v-bind:class="{ hidden: curentlyLogin }">
 			<form onsubmit="return false;" id="connect" class="w-full h-1/2 rounded-3xl bg-slate-200 p-4 flex-col flex items-center mb-5 mt-5">
 				<h3>Connect or register to Onearth :</h3>
-				<input type="text" name="username" id="usernamec" v-model="usernameInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your username" />
-				<input type="password" name="password" id="password" v-model="passwordInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your password" />
+				<label for="username">Username :</label><br />
+				<input type="text" name="username" id="username" v-model="usernameInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your username" maxlength="30" size="3" @input="verifDirectFill()" v-bind:class="{ 'bg-red-200': errorUsername }" />
+				<label for="password">Password :</label><br />
+				<input type="password" name="password" id="password" v-model="passwordInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your password" maxlength="100" size="3" />
 				<div class="w-full flex flex-raw justify-center text-xl">
-					<input type="submit" value="Sign in" class="m-3 w-1/4 rounded-md bg-lime-700 p-2 active:bg-lime-800" @click="login()" />
-					<input type="submit" value="Sign up" class="m-3 w-1/4 rounded-md bg-cyan-600 p-2 active:bg-cyan-700" />
+					<input type="submit" value="Sign in" class="m-3 w-1/4 rounded-md bg-lime-700 p-2 active:bg-lime-800" @click="login()" v-bind:disabled="errorUsername" v-bind:class="{ 'bg-red-200': errorUsername }" />
+					<input type="submit" value="Sign up" class="m-3 w-1/4 rounded-md bg-cyan-600 p-2 active:bg-cyan-700" @click="create()" v-bind:class="{ 'bg-red-200': errorUsername }" v-bind:disabled="errorUsername" />
 				</div>
 			</form>
 		</div>
 		<!-- Quand login -->
 		<div v-bind:class="{ hidden: !curentlyLogin }">
 			<form onsubmit="return false;" class="w-full h-1/2 rounded-3xl bg-slate-200 p-4 flex-col flex items-center mb-5 mt-5">
-				<div class="h-full flex flex-col justify-end">
+				<div class="h-full flex flex-col justify-end w-4/5">
 					<div class="form-control">
 						<label class="label cursor-pointer">
-							<span class="label-text mr-4 text-xl">Compte privé</span>
+							<span class="label-text mr-4 text-xl">Private account : </span>
 							<input type="checkbox" class="toggle toggle-lg bg-slate-500 checked:bg-lime-700" checked />
 						</label>
 					</div>
@@ -53,6 +57,11 @@
 						</label>
 					</div>
 				</div>
+				<input type="submit" value="Save change" class="m-2 w-1/2 rounded-md bg-lime-700 p-2 active:bg-lime-800" />
+			</form>
+			<form onsubmit="return false;" class="w-full h-1/2 rounded-3xl bg-slate-200 p-4 flex-col flex items-center mb-5 mt-5">
+				<input type="text" name="username" id="username" v-model="usernameInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your new username" maxlength="30" size="3" />
+				<input type="text" name="bio" id="bio" v-model="bioInput" class="m-2 w-5/6 rounded-md p-1" placeholder="Your new bio" maxlength="300" />
 				<input type="submit" value="Save change" class="m-2 w-1/2 rounded-md bg-lime-700 p-2 active:bg-lime-800" />
 			</form>
 			<div class="w-full h-1/2 rounded-3xl bg-slate-200 p-4 flex-col flex items-center mb-5 mt-5">
@@ -71,9 +80,14 @@ export default {
 		return {
 			usernameInput: "",
 			passwordInput: "",
+			username: "",
+			bioInput: "",
 			curentlyLogin: true,
-			succesMessage: false,
-			errorMessage: false,
+			succesMessageState: false,
+			succesMessage: "Wait a text",
+			errorMessageState: false,
+			errorMessage: "",
+			errorUsername: true,
 			loading: false,
 		};
 	},
@@ -84,7 +98,6 @@ export default {
 	methods: {
 		async verifLogin() {
 			if (localStorage.getItem("token") == null) {
-				console.log("stop");
 				this.curentlyLogin = false;
 				return;
 			}
@@ -93,36 +106,110 @@ export default {
 
 			if (loginResApi.data.state == "true") {
 				this.curentlyLogin = true;
-				console.log("relog with " + localStorage.getItem("token"));
+				this.username = loginResApi.data.username;
 			} else {
 				this.curentlyLogin = false;
-				localStorage.removeItem("token");
+				return localStorage.removeItem("token");
 			}
+
+			this.loading = true;
+			const userResApi = await axios.get("http://api.cleboost.ovh/onearth/user/getInfo.php?username=" + this.username);
+			this.loading = false;
+			this.username = userResApi.data[0].username;
+			this.pictureLink = userResApi.data[0].picturelink;
+			this.bioInput = userResApi.data[0].bio;
+			this.badge = userResApi.data[0].badge;
 		},
 		async login() {
-			console.log("envoie request en cours");
 			this.loading = true;
 			const loginResApi = await axios.get("http://api.cleboost.ovh/onearth/user/getToken.php?username=" + this.usernameInput + "&password=" + this.passwordInput);
-			console.log(loginResApi.data);
 			this.loading = false;
 			if (loginResApi.data.state == "true") {
 				this.curentlyLogin = true;
 				localStorage.setItem("token", loginResApi.data.token);
-				console.log("log with " + loginResApi.data.token);
-				// Message de succes
+				this.succesMessage = "Connection is successfull !";
+				this.succesMessageState = true;
+				//Wait 3 sec
+				setTimeout(() => {
+					this.succesMessageState = false;
+				}, 3000);
 			} else {
+				this.errorMessage = "Error of login";
+				this.errorMessageState = true;
+				setTimeout(() => {
+					this.errorMessageState = false;
+				}, 3000);
 				this.curentlyLogin = false;
 				// Message d'erreur
 			}
+			this.verifLogin();
 		},
 		async unLogin() {
 			const loginResApi = await axios.get("http://api.cleboost.ovh/onearth/user/unLogin.php?token=" + localStorage.getItem("token"));
 			if (loginResApi.data.state == "true") {
 				localStorage.removeItem("token");
 				this.curentlyLogin = false;
+				this.succesMessage = "You have been successfully disconnected !";
+				this.succesMessageState = true;
+				setTimeout(() => {
+					this.succesMessageState = false;
+				}, 3000);
 				// Message de succes
 			} else {
-				// Message d'erreur
+				this.errorMessage = "Error of unlogin";
+				this.errorMessageState = true;
+				setTimeout(() => {
+					this.errorMessageState = false;
+				}, 3000);
+			}
+		},
+		async create() {
+			//Vérifie si les champs sont remplis
+			if (this.usernameInput == "" || this.passwordInput == "") {
+				this.errorMessage = "Please fill all the fields";
+				this.errorMessageState = true;
+				setTimeout(() => {
+					this.errorMessageState = false;
+				}, 3000);
+				return;
+			}
+
+			//Vérifie avec regex si les pseudo n'ont pas de caractères spéciaux
+			if (!this.usernameInput.match(/^[a-zA-Z0-9]+$/)) {
+				this.errorMessage = "Your username can't have special characters";
+				this.errorMessageState = true;
+				setTimeout(() => {
+					this.errorMessageState = false;
+				}, 3000);
+				return;
+			}
+
+			this.loading = true;
+			const loginResApi = await axios.get(`http://api.cleboost.ovh/onearth/user/createAcount.php?username=${this.usernameInput}&password=${this.passwordInput}`);
+			this.loading = false;
+			if (loginResApi.data.state == "true") {
+				this.succesMessage = "Your account has been created !";
+				this.succesMessageState = true;
+				setTimeout(() => {
+					this.succesMessageState = false;
+				}, 3000);
+				// Message de succes
+			} else {
+				this.errorMessage = loginResApi.data.moreInfo;
+				this.errorMessageState = true;
+				setTimeout(() => {
+					this.errorMessageState = false;
+				}, 3000);
+			}
+		},
+		async verifDirectFill() {
+			//Vérifie avec regex si les pseudo n'ont pas de caractères spéciaux et le nombre de caractères
+			this.errorUsername = false;
+			if (this.usernameInput.length < 3 || this.usernameInput.length > 30) {
+				this.errorUsername = true;
+			}
+			if (!this.usernameInput.match(/^[a-zA-Z0-9]+$/)) {
+				this.errorUsername = true;
 			}
 		},
 	},
@@ -133,65 +220,73 @@ export default {
 $anime-time: 5s;
 
 $box-size: 300px;
-$clip-distance: .05;
+$clip-distance: 0.05;
 $clip-size: $box-size * (1 + $clip-distance * 2);
 $path-width: 2px;
 
 $main-color: #69ca62;
 
-$codepen-logo-path: url('http://localhost:5173/icon.png');
+$codepen-logo-path: url("http://localhost:5173/icon.png");
 
 %full-fill {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
 }
 
-.bb {
-  @extend %full-fill;
-  width: $box-size;
-  height: $box-size;
-  margin: auto;
-  background: $codepen-logo-path no-repeat 50% / 70% rgba(#000, .1);
-  color: $main-color;
-  box-shadow: inset 0 0 0 1px rgba($main-color, .5);
+.loading {
+	@extend %full-fill;
+	width: $box-size;
+	height: $box-size;
+	margin: auto;
+	background: $codepen-logo-path no-repeat 50% / 70% rgba(#000, 0.1);
+	color: $main-color;
+	box-shadow: inset 0 0 0 1px rgba($main-color, 0.5);
 
-  &::before,
-  &::after {
-    @extend %full-fill;
-    content: '';
-    z-index: -1;
-    margin: -1 * $clip-distance * 100%;
-    box-shadow: inset 0 0 0 $path-width; 
-    animation: clipMe $anime-time linear infinite;
-  }
+	&::before,
+	&::after {
+		@extend %full-fill;
+		content: "";
+		z-index: -1;
+		margin: -1 * $clip-distance * 100%;
+		box-shadow: inset 0 0 0 $path-width;
+		animation: clipMe $anime-time linear infinite;
+	}
 
-  &::before {
-    animation-delay: $anime-time * -.5;
-  }
+	&::before {
+		animation-delay: $anime-time * -0.5;
+	}
 
-  // for debug
-//   &:hover {
-//     &::after,
-//     &::before {
-//       background-color: rgba(#f00, .3);
-//     }
-//   }
-
+	// for debug
+	//   &:hover {
+	//     &::after,
+	//     &::before {
+	//       background-color: rgba(#f00, .3);
+	//     }
+	//   }
 }
 
 @keyframes clipMe {
-  0%, 100% {clip: rect(0px, $clip-size, $path-width, 0px); }
-  25% {clip: rect(0px, $path-width, $clip-size, 0px); }
-  50% {clip: rect($clip-size - $path-width, $clip-size, $clip-size, 0px); }
-  75% {clip: rect(0px, $clip-size, $clip-size, $clip-size - $path-width); }
+	0%,
+	100% {
+		clip: rect(0px, $clip-size, $path-width, 0px);
+	}
+	25% {
+		clip: rect(0px, $path-width, $clip-size, 0px);
+	}
+	50% {
+		clip: rect($clip-size - $path-width, $clip-size, $clip-size, 0px);
+	}
+	75% {
+		clip: rect(0px, $clip-size, $clip-size, $clip-size - $path-width);
+	}
 }
 
 *,
 *::before,
 *::after {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 </style>
