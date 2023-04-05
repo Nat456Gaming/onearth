72472ab2cfa77d441ee43a458a2baf0ede008a29
Cleboost <clement.balarot@gmail.com>
2023-04-05 18:40:30 +0200

    change marge

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,8 +1,8 @@
 <template>
    <nav class=" p-6 flex flex-raw bg-[#252525] text-white h-[15%] w-full fixed bottom-0 justify-center items-center">
-      <RouterLink to="/" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[11vh]">home</span></RouterLink>
-      <RouterLink to="/search/user" class="flex w-1/3 justify-center"><span class="material-icons-round text-[11vh]">search</span></RouterLink>
-      <RouterLink to="/friends" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[11vh]">perm_identity</span></RouterLink>
+      <RouterLink to="/" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">home</span></RouterLink>
+      <RouterLink to="/search/user" class="flex w-1/3 justify-center"><span class="material-icons-round text-[10vh]">search</span></RouterLink>
+      <RouterLink to="/friends" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">perm_identity</span></RouterLink>
    </nav>
 </template>
 
