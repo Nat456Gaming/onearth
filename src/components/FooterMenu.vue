c4b41bf16d08e056a02e3245ef65fb984e1e3f18
Youritch <86657843+Youritch@users.noreply.github.com>
2023-05-22 21:41:16 +0200

    tentatives de regler pb

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,15 +1,15 @@
 <template>
-   <nav class=" p-6 flex flex-raw bg-[#252525] text-white h-[15%] w-full fixed bottom-0 justify-center items-center">
-      <RouterLink to="/" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">home</span></RouterLink>
-      <RouterLink to="/search/user" class="flex w-1/3 justify-center"><span class="material-icons-round text-[10vh]">search</span></RouterLink>
-      <RouterLink to="/friends" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">perm_identity</span></RouterLink>
-   </nav>
+	<nav class="p-6 flex flex-raw bg-[#252525] text-white h-[15%] w-full fixed bottom-0 justify-center items-center">
+		<RouterLink to="/" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">home</span></RouterLink>
+		<RouterLink to="/search/user" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">search</span></RouterLink>
+		<RouterLink to="/friends" class="flex w-1/3 justify-center" active-class="activeRoute"><span class="material-icons-round text-[10vh]">perm_identity</span></RouterLink>
+	</nav>
 </template>
 
 <style scoped>
-  .activeRoute {
-    color: #42b983;
-    transform: scale(1.2);
-    transition: 0.2s
-  }
+.activeRoute {
+	color: #42b983;
+	transform: scale(1.2);
+	transition: 0.2s;
+}
 </style>
